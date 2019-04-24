9024899d2982e01f24acba1f99ff5f677f56f6af
Koen Aers <koen.aers@gmail.com>
2019-04-24 21:09:42 +0200

    QE-32: Add a test for ProjectUtils.projectExists(String)
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/io.quarkus.eclipse.core.test/src/io/quarkus/eclipse/core/ProjectUtilsTest.java
+++ test/io.quarkus.eclipse.core.test/src/io/quarkus/eclipse/core/ProjectUtilsTest.java
@@ -21,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -42,6 +43,17 @@ public class ProjectUtilsTest {
 				"1.0.0-SHAPSHOT", 
 				null);
 		assertTrue(project.exists());
+	}
+	
+	@Test 
+	public void testProjectExists() throws Exception {
+		assertFalse(ProjectUtils.projectExists("com.acme.quarkus"));
+		ResourcesPlugin
+				.getWorkspace()
+				.getRoot()
+				.getProject("com.acme.quarkus")
+				.create(new NullProgressMonitor());;
+		assertTrue(ProjectUtils.projectExists("com.acme.quarkus"));
 	}
 	
 }

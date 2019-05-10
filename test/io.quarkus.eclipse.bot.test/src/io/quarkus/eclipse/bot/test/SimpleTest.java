9f458f3c88a34dd63b2565fd5225e61d5e17a356
Koen Aers <koen.aers@gmail.com>
2019-05-10 17:07:07 +0200

    QE-49: Set up a Reddeer plugin for automatic testing - Add needed tycho configuration for the test to run
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- test/io.quarkus.eclipse.bot.test/src/io/quarkus/eclipse/bot/test/SimpleTest.java
+++ test/io.quarkus.eclipse.bot.test/src/io/quarkus/eclipse/bot/test/SimpleTest.java
@@ -4,14 +4,12 @@ package io.quarkus.eclipse.bot.test;
 import static org.junit.Assert.assertTrue;
 
 import org.eclipse.reddeer.junit.runner.RedDeerSuite;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(RedDeerSuite.class)
 public class SimpleTest {
 
-	@Ignore
 	@Test
 	public void test() {
 		assertTrue(true);

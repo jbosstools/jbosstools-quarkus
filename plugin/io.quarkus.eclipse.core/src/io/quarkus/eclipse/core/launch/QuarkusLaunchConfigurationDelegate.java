b91f1b5257ae96c34a55ab7483f05efb9778b5d7
Koen Aers <koen.aers@gmail.com>
2019-05-25 13:51:06 +0200

    QE-70: Create a Quarkus launch configuration - QuarkusLaunchConfigurationDelegate is a subclass of JavaLaunchDelegate
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- plugin/io.quarkus.eclipse.core/src/io/quarkus/eclipse/core/launch/QuarkusLaunchConfigurationDelegate.java
+++ plugin/io.quarkus.eclipse.core/src/io/quarkus/eclipse/core/launch/QuarkusLaunchConfigurationDelegate.java
@@ -20,14 +20,15 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.jdt.launching.JavaLaunchDelegate;
 
-public class QuarkusLaunchConfigurationDelegate implements ILaunchConfigurationDelegate {
+public class QuarkusLaunchConfigurationDelegate extends JavaLaunchDelegate {
 
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
 			throws CoreException {
-		System.out.println("Hello, world");
+		System.out.println("Launching Quarkus Application");
+		super.launch(configuration, mode, launch, monitor);
 	}
 
 }

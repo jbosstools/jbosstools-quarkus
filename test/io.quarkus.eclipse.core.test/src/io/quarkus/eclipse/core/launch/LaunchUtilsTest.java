1cf963eafc6bc1408942fe127a0f369bf7742476
Koen Aers <koen.aers@gmail.com>
2019-06-07 10:50:42 +0200

    QE-70: Create a Quarkus launch configuration - Create a test for LaunchUtils#initializeQuarkusLaunchConfiguration(...)
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ test/io.quarkus.eclipse.core.test/src/io/quarkus/eclipse/core/launch/LaunchUtilsTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.quarkus.eclipse.core.launch;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.dev.DevModeMain;
+
+public class LaunchUtilsTest {
+	
+	private Object[] arguments = null;
+	
+	@Test
+	public void testInitializeQuarkusLaunchConfiguration() {
+		ILaunchConfigurationWorkingCopy lcwc = (ILaunchConfigurationWorkingCopy)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[] { ILaunchConfigurationWorkingCopy.class }, 
+				new TestLaunchConfigurationWorkingCopyInvocationHandler());
+		assertNull(arguments);
+		LaunchUtils.initializeQuarkusLaunchConfiguration(lcwc);
+		assertEquals(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, arguments[0]);
+		assertEquals(DevModeMain.class.getName(), arguments[1]);
+	}
+	
+	private class TestLaunchConfigurationWorkingCopyInvocationHandler implements InvocationHandler {
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			arguments = args;
+			return null;
+		}		
+	}
+	
+}
+

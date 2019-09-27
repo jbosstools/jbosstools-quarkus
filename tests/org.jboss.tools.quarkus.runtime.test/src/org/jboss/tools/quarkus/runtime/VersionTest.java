a52bd84b93faa1acbdadb94184f7004d610351a6
Koen Aers <koen.aers@gmail.com>
2019-09-27 14:27:46 +0200

    JBIDE-26866: Update Quarkus dependency to release 0.23.0 - Update dependency to 0.23.0
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tests/org.jboss.tools.quarkus.runtime.test/src/org/jboss/tools/quarkus/runtime/VersionTest.java
+++ tests/org.jboss.tools.quarkus.runtime.test/src/org/jboss/tools/quarkus/runtime/VersionTest.java
@@ -24,7 +24,7 @@ public class VersionTest {
 	
 	@Test
 	public void testVersion() {
-		assertEquals("0.22.0", io.quarkus.maven.utilities.MojoUtils.getPluginVersion());
+		assertEquals("0.23.0", io.quarkus.maven.utilities.MojoUtils.getPluginVersion());
 	}
 
 }

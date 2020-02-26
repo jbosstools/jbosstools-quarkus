739127e81f5f7b3f1ec5602b3d5e0c726c0591b1
Jeff MAURY <jmaury@redhat.com>
2020-02-26 16:00:03 +0100

    JBIDE-27077: Extensions with status other than preview should get status displayed
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- tests/org.jboss.tools.quarkus.core.test/src/org/jboss/tools/quarkus/core/code/model/QuarkusExtensionsTest.java
+++ tests/org.jboss.tools.quarkus.core.test/src/org/jboss/tools/quarkus/core/code/model/QuarkusExtensionsTest.java
@@ -44,4 +44,13 @@ public class QuarkusExtensionsTest {
         assertEquals("Amazon DynamoDB client", model.getCategories().get(0).getExtensions().get(0).getName());
         assertEquals("Amazon DynamoDB client (Preview)", model.getCategories().get(0).getExtensions().get(0).asLabel());
     }
+    
+    @Test
+    public void checkExperimentalExtension() throws IOException {
+        QuarkusModel model = new QuarkusModel(load("/single-experimental-extension.json"));
+        assertEquals(1, model.getCategories().size());
+        assertEquals(1, model.getCategories().get(0).getExtensions().size());
+        assertEquals("Amazon DynamoDB client", model.getCategories().get(0).getExtensions().get(0).getName());
+        assertEquals("Amazon DynamoDB client (Experimental)", model.getCategories().get(0).getExtensions().get(0).asLabel());
+    }
 }

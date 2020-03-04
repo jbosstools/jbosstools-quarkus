147806fca6f20706bd7cecb8ea64b79e5c53e417
Jeff MAURY <jmaury@redhat.com>
2020-03-04 17:34:00 +0100

    JBIDE-27057: rootProject.name in Gradle Project is incorrect
    - Added additional checks for Gradle projects
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- plugins/org.jboss.tools.quarkus.ui/src/org/jboss/tools/quarkus/ui/wizard/CodeProjectWizard.java
+++ plugins/org.jboss.tools.quarkus.ui/src/org/jboss/tools/quarkus/ui/wizard/CodeProjectWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Red Hat, Inc.
+ * Copyright (c) 2019-2020 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution,
@@ -10,12 +10,16 @@
  ******************************************************************************/
 package org.jboss.tools.quarkus.ui.wizard;
 
+import java.util.Objects;
+
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 import org.jboss.tools.common.ui.wizard.AbstractModelWizard;
+import org.jboss.tools.quarkus.core.code.model.Tool;
 
 public class CodeProjectWizard extends AbstractModelWizard<CodeProjectModel> implements INewWizard {
 
@@ -39,9 +43,21 @@ public class CodeProjectWizard extends AbstractModelWizard<CodeProjectModel> imp
 
     @Override
     public boolean performFinish() {
-        Job.create("Creating Quarkus project...", 
-                new CodeProjectWizardController(getModel())::run).schedule();
-        return true;
+    	boolean valid = isValid();
+        if (valid) {
+			Job.create("Creating Quarkus project...", new CodeProjectWizardController(getModel())::run).schedule();
+		}
+		return valid;
     }
+
+	private boolean isValid() {
+		CodeProjectModel model = getModel();
+		boolean valid = true;
+		if (Tool.GRADLE == model.getTool() && !Objects.equals(model.getArtifactId(), model.getProjectName())) {
+		  ((WizardPage)getContainer().getCurrentPage()).setErrorMessage("Gradle project name and artifact id must be identical");
+		  valid = false;
+		}
+		return valid;
+	}
 
 }

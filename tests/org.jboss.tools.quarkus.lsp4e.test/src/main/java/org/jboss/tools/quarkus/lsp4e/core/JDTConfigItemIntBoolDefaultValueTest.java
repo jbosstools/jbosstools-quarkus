7c4f73621ac1a5463352232c4b50a6046be5bcbc
Jeff MAURY <jmaury@redhat.com>
2020-07-21 15:39:01 +0200

    JBIDE-27425: quarkus build fails on Java8 (#105)
    * JBIDE-27425: quarkus build fails on Java8
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- tests/org.jboss.tools.quarkus.lsp4e.test/src/main/java/org/jboss/tools/quarkus/lsp4e/core/JDTConfigItemIntBoolDefaultValueTest.java
+++ tests/org.jboss.tools.quarkus.lsp4e.test/src/main/java/org/jboss/tools/quarkus/lsp4e/core/JDTConfigItemIntBoolDefaultValueTest.java
@@ -1,8 +1,9 @@
 package org.jboss.tools.quarkus.lsp4e.core;
 
-import org.eclipse.lsp4mp.jdt.core.ConfigItemIntBoolDefaultValueTest;
 import org.jboss.tools.quarkus.lsp4e.internal.JDTUtilsImpl;
 import org.junit.BeforeClass;
+
+import com.redhat.microprofile.jdt.quarkus.ConfigItemIntBoolDefaultValueTest;
 
 public class JDTConfigItemIntBoolDefaultValueTest extends ConfigItemIntBoolDefaultValueTest {
 	@BeforeClass

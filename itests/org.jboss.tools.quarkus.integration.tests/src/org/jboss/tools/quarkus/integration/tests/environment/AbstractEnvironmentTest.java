57f3455258d314ecfb9e2667aa035883b35661ba
Oleksii Korniienko <olkornii@redhat.com>
2021-05-31 09:56:23 +0200

    JBIDE-27821: reworked "addEnvironment" due to https://github.com/eclipse/reddeer/pull/2120
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- itests/org.jboss.tools.quarkus.integration.tests/src/org/jboss/tools/quarkus/integration/tests/environment/AbstractEnvironmentTest.java
+++ itests/org.jboss.tools.quarkus.integration.tests/src/org/jboss/tools/quarkus/integration/tests/environment/AbstractEnvironmentTest.java
@@ -13,16 +13,12 @@ package org.jboss.tools.quarkus.integration.tests.environment;
 import org.eclipse.reddeer.common.wait.TimePeriod;
 import org.eclipse.reddeer.common.wait.WaitUntil;
 import org.eclipse.reddeer.eclipse.condition.ConsoleHasText;
+import org.eclipse.reddeer.eclipse.debug.ui.launchConfigurations.EnvironmentTab;
 import org.eclipse.reddeer.eclipse.debug.ui.launchConfigurations.RunConfigurationsDialog;
 import org.eclipse.reddeer.eclipse.ui.console.ConsoleView;
-import org.eclipse.reddeer.swt.impl.button.OkButton;
-import org.eclipse.reddeer.swt.impl.button.PushButton;
-import org.eclipse.reddeer.swt.impl.ctab.DefaultCTabItem;
 import org.eclipse.reddeer.swt.impl.shell.DefaultShell;
-import org.eclipse.reddeer.swt.impl.text.LabeledText;
 import org.jboss.tools.quarkus.integration.tests.launch.configuration.AbstractLaunchConfigurationTest;
 import org.jboss.tools.quarkus.reddeer.common.QuarkusLabels.Shell;
-import org.jboss.tools.quarkus.reddeer.common.QuarkusLabels.TextLabels;
 import org.jboss.tools.quarkus.reddeer.ui.launch.QuarkusLaunchConfigurationTabGroup;
 
 /**
@@ -49,11 +45,9 @@ public abstract class AbstractEnvironmentTest extends AbstractLaunchConfiguratio
 			QuarkusLaunchConfigurationTabGroup launchConfiguration) {
 		runDialog.open();
 		runDialog.select(launchConfiguration, projectName);
-		new DefaultCTabItem("Environment").activate();
-		new PushButton("Add...").click();
-		new LabeledText(TextLabels.NAME).setText(ENVIRONMENT_NAME);
-		new LabeledText(TextLabels.VALUE).setText("true");
-		new OkButton().click();
+		EnvironmentTab envTab = new EnvironmentTab();
+		envTab.activate();
+		envTab.add(ENVIRONMENT_NAME, "true");
 		new DefaultShell(Shell.RUN_CONFIGURATION).setFocus();
 		runDialog.close(true);
 	}

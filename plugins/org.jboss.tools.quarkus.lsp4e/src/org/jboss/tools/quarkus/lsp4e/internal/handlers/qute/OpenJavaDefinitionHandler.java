5c85f41a4a99a429e881e101f56854d0ced55431
Jeff MAURY <jmaury@redhat.com>
2022-05-19 17:37:43 +0200

    JBIDE-28451: Navigation from Qute template to a method does not work (#175)
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- plugins/org.jboss.tools.quarkus.lsp4e/src/org/jboss/tools/quarkus/lsp4e/internal/handlers/qute/OpenJavaDefinitionHandler.java
+++ plugins/org.jboss.tools.quarkus.lsp4e/src/org/jboss/tools/quarkus/lsp4e/internal/handlers/qute/OpenJavaDefinitionHandler.java
@@ -10,23 +10,24 @@
  ******************************************************************************/
 package org.jboss.tools.quarkus.lsp4e.internal.handlers.qute;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.lsp4e.LSPEclipseUtils;
 import org.eclipse.lsp4e.command.LSPCommandHandler;
 import org.eclipse.lsp4j.Command;
-import org.eclipse.ui.PartInitException;
+import org.eclipse.lsp4j.Location;
+import org.eclipse.ui.PlatformUI;
 import org.jboss.tools.quarkus.lsp4e.QuarkusLSPPlugin;
+import org.jboss.tools.quarkus.lsp4e.internal.ls.JDTUtilsImpl;
+
+import com.redhat.qute.commons.QuteJavaDefinitionParams;
+import com.redhat.qute.jdt.QuteSupportForTemplate;
 
 /**
  * @author Red Hat Developers
@@ -34,64 +35,51 @@ import org.jboss.tools.quarkus.lsp4e.QuarkusLSPPlugin;
  */
 public class OpenJavaDefinitionHandler extends LSPCommandHandler {
 	
-	private static final String PROJECT_PARAMETER = "projectUri";
-	
-	private static final String TYPE_PARAMETER = "sourceType";
-	
-	private static final String FIELD_PARAMETER = "sourceField";
-	
-	private static final String METHOD_PARAMETER = "sourceMethod";
+    private static final String PROJECT_URI_ATTR = "projectUri";
+    private static final String SOURCE_TYPE_ATTR = "sourceType";
+    private static final String SOURCE_FIELD_ATTR = "sourceField";
+    private static final String SOURCE_METHOD_ATTR = "sourceMethod";
+    private static final String SOURCE_PARAMETER_ATTR = "sourceParameter";
+    private static final String DATA_METHOD_INVOCATION_ATTR = "dataMethodInvocation";
 
 	@Override
 	public Object execute(ExecutionEvent event, Command command, IPath path) throws ExecutionException {
 		try {
-			if (!command.getArguments().isEmpty() && command.getArguments().get(0) instanceof Map<?, ?>) {
-				IJavaElement element = getJavaElement((Map<String, String>) command.getArguments().get(0));
-				if (element != null) {
-					JavaUI.openInEditor(element);
-				}
+			QuteJavaDefinitionParams params = getQuteJavaDefinitionParams(command.getArguments());
+			if (params != null) {
+				Location location = QuteSupportForTemplate.getInstance().getJavaDefinition(params, JDTUtilsImpl.getInstance(), new NullProgressMonitor());
+				LSPEclipseUtils.openInEditor(location, PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage());
 			}
-		} catch (PartInitException | JavaModelException e) {
+		} catch (CoreException e) {
 			QuarkusLSPPlugin.logException(e.getLocalizedMessage(), e);
 		}
 		return null;
 	}
 
-	/**
-	 * @param parameters
-	 * @return
-	 */
-	private IJavaElement getJavaElement(Map<String, String> parameters) {
-		try {
-			String projectName = parameters.get(PROJECT_PARAMETER);
-			if (projectName != null) {
-				IProject project =ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
-				if (project != null && project.isAccessible()) {
-					IJavaProject javaProject = JavaCore.create(project);
-					String typeName = parameters.get(TYPE_PARAMETER);
-					if (javaProject != null && typeName != null) {
-						IType type = javaProject.findType(typeName);
-						if (type != null) {
-							IJavaElement element = null;
-							String name = parameters.get(METHOD_PARAMETER);
-							if (name == null) {
-								name = parameters.get(FIELD_PARAMETER);
-								if (name != null) {
-									element = type.getField(name);
-								}
-							} else {
-								final String fname = name;
-								element = Arrays.stream(type.getMethods()).filter(m -> fname.equals(m.getElementName())).findFirst().orElse(null);
-							}
-							return element;
-						}
-					}
-				}
-			}
-		} catch (JavaModelException e) {
-			QuarkusLSPPlugin.logException(e.getLocalizedMessage(), e);
-		}
-		return null;
-	}
+    protected String getString(String name, Map<String, Object> obj) {
+        return obj.containsKey(name)?obj.get(name).toString():null;
+    }
 
+    protected boolean getBoolean(String name, Map<String, Object> obj) {
+        return obj.containsKey(name)?(boolean) obj.get(name):false;
+    }
+
+    private QuteJavaDefinitionParams getQuteJavaDefinitionParams(List<Object> arguments) {
+        if (!arguments.isEmpty() && arguments.get(0) instanceof Map) {
+            Map<String, Object> obj = ((Map<String, Object>) arguments.get(0));
+            String templateFileUri = getString(PROJECT_URI_ATTR, obj);
+            String sourceType = getString(SOURCE_TYPE_ATTR, obj);
+            QuteJavaDefinitionParams params = new QuteJavaDefinitionParams(sourceType, templateFileUri);
+            String sourceField = getString(SOURCE_FIELD_ATTR, obj);
+            params.setSourceField(sourceField);
+            String sourceMethod = getString(SOURCE_METHOD_ATTR, obj);
+            params.setSourceMethod(sourceMethod);
+            String methodParameter = getString(SOURCE_PARAMETER_ATTR, obj);
+            params.setSourceParameter(methodParameter);
+            boolean dataMethodInvocation = getBoolean(DATA_METHOD_INVOCATION_ATTR, obj);
+            params.setDataMethodInvocation(dataMethodInvocation);
+            return params;
+        }
+        return null;
+    }
 }

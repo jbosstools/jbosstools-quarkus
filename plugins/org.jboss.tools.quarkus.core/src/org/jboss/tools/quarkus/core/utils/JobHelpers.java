3969d79476e30231b382d0b049389cc9d8e5fc15
Jeff MAURY <jmaury@redhat.com>
2022-07-04 14:22:33 +0200

    JBIDE-28504: Remove dependency from JDT.LS
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- plugins/org.jboss.tools.quarkus.core/src/org/jboss/tools/quarkus/core/utils/JobHelpers.java
+++ plugins/org.jboss.tools.quarkus.core/src/org/jboss/tools/quarkus/core/utils/JobHelpers.java
@@ -8,7 +8,7 @@
  * Contributors:
  *      Sonatype, Inc. - initial API and implementation
  *******************************************************************************/
-package org.jboss.tools.quarkus.core.code.utils;
+package org.jboss.tools.quarkus.core.utils;
 
 import java.util.ArrayList;
 import java.util.Deque;
